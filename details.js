d5f636b88b4a75ea964cc63dca223eeed2edfdd5
Ahmed Saeed <ahmedsaeed2462007@gmail.com>
2026-05-02 22:39:19 +0300

    Update details.js

--- details.js
+++ details.js
@@ -377,7 +377,7 @@ function displayProgramDetails() {
 
 // دالة الحجز
 function bookProgram(programName) {
-    var bookingUrl = "book-a-toor.html?destination=" + encodeURIComponent(programName);
+    var bookingUrl = "book-a-toor.html?destinations=" + encodeURIComponent(programName);
     window.location.href = bookingUrl;
 }
 
