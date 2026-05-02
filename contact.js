1ba683fe6f0fb6bd0cd54a0f5fbb47e47ec8273b
Ahmed-Wael2007-Egy <ahmedwael1662007@gmail.com>
2026-05-03 01:32:30 +0300

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,38 @@
+let form = document.forms;
+let phone = document.getElementById('phone');
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let submit = document.getElementById('submit');
+let emailError = document.getElementById('emailError');
+let phoneError = document.getElementById('phoneError');
+// إعادة تعيين الرسايل (عشان لو الخطأ اتصلح تختفي)
+submit.onclick = function (e) {
+    emailError.style.display = 'none';
+    phoneError.style.display = 'none';
+    let check = true;
+    // check email
+    if (email.value === "") {
+        emailError.textContent = "Email is required";
+        emailError.style.display = 'block';
+        check = false;
+    }
+    else if (!email.value.includes('/^[^\s@]+@[^\s@]+\.[^\s@]+$/')) {
+        emailError.textContent = "Please enter a valid email (e.g. name@mail.com)";
+        emailError.style.display = 'block';
+        check = false;
+    }
+    // check phone
+    if (phone.value === "") {
+        phoneError.textContent = "Phone number is required";
+        phoneError.style.display = 'block';
+        check = false;
+    }
+    else if (phone.value.length < 11) {
+        phoneError.textContent = "Phone number must be at least 11 digits";
+        phoneError.style.display = 'block';
+        check = false;
+    }
+    if (check === false) {
+        e.preventDefault();
+    }
+}
