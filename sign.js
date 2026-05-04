f12ee7d53d4ea9eb096089ac7040ed19f8a942f0
Ahmed Saeed <ahmedsaeed2462007@gmail.com>
2026-05-04 22:52:08 +0300

    Implement form submission validation
    Add form validation for email and password fields

--- sign.js
+++ sign.js
@@ -22,3 +22,30 @@ if (toggle) {
         nav.classList.toggle("active");
     });
 }
+
+// نجيب الفورم
+const form = document.querySelector(".glass-box");
+
+// نسمع حدث submit
+form.addEventListener("submit", function (x) {
+
+    // نجيب القيم
+    const email = form.querySelector("input[type='email']").value;
+    const password = form.querySelector("input[type='password']").value;
+
+    // validation بسيط
+    if (email === "" || !email.includes("@")) {
+        alert("Please enter a valid email");
+        x.preventDefault(); // يمنع الإرسال
+        return;
+    }
+
+    if (password === "" || password.length < 6) {
+        alert("Password must be at least 6 characters");
+        e.preventDefault();
+        return;
+    }
+
+    // لو كله تمام
+    alert("Login successful ✅");
+});
