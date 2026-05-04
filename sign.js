15415a8111b2917b4356c727c9482730c55e621a
Ahmed-Wael2007-Egy <ahmedwael1662007@gmail.com>
2026-05-05 01:26:06 +0300

    Update sign.js

--- sign.js
+++ sign.js
@@ -1,28 +1,20 @@
 document.querySelectorAll("form").forEach(form => {
-    form.addEventListener("submit", function(e) {
-
-        // لو الفورم بتاع login (glass-box)
+    form.addEventListener("submit", function (e) {
         if (form.classList.contains("glass-box")) {
-
             const email = form.querySelector("input[type='email']").value;
             const password = form.querySelector("input[type='password']").value;
-
             if (email === "" || !email.includes("@")) {
                 alert("Please enter a valid email");
                 e.preventDefault();
                 return;
             }
-
             if (password === "" || password.length < 6) {
                 alert("Password must be at least 6 characters");
                 e.preventDefault();
                 return;
             }
-
             alert("Login successful ✅");
-        } 
-        
-        // باقي الفورمات (زي forgot password)
+        }
         else {
             e.preventDefault();
             alert("تم الإرسال بنجاح ✈️");
