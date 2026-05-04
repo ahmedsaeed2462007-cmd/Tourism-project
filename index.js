917736f2db1bc87facadd1972d36d21e3c8e2e64
IbrahimAtef1925 <ibrahim.atef1922@gmail.com>
2026-05-04 17:47:09 +0300

    cleaned index js file

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-// 1. Find the image and the dropdown menu on the page
 let bgImage = document.getElementById("main-bg");
 let bgSelector = document.getElementById("bg-selector");
 
-// 2. Check if the user already saved a background preference in their browser
 let savedBg = localStorage.getItem("userBackground");
 
-// 3. If they do have a saved background, apply it right when the page loads
 if (savedBg !== null) {
     bgImage.src = savedBg;
-    bgSelector.value = savedBg; // Make sure the dropdown displays the correct name
+    bgSelector.value = savedBg;
 }
 
-// 4. Listen for when the user picks a new option from the dropdown menu
 bgSelector.addEventListener("change", function () {
-    let newImage = bgSelector.value; // Get the path of the image they just clicked
+    let newImage = bgSelector.value;
 
-    bgImage.src = newImage;          // Change the picture on the screen
+    bgImage.src = newImage;
 
-    // Save this new choice to the browser's memory so it remembers for next time
     localStorage.setItem("userBackground", newImage);
 });
